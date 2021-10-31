caaa013123a42b849280c6ed844410c86191fbd2
Viresh Doshi <vireshdoshi@gmail.com>
2021-10-31 22:08:20 +0000

    new project files

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # python-flask-upskill
 Python Flask Upskilling Project  
+
+## Description
+The aim of this project is to introduce you to the following Python packages:
+- sql alchemy
+- marshmallow
+- flask 
+- celery 
+
+
+### Versions
+```bash
+$ python --version
+Python 3.9.2 
+```
+
+### Commands 
+```bash
+$ pip3 install pipenv
+$ pipenv install flask flask-sqlalchemy flask-marshmallow flask-sqlalchemy marshmallow-sqlalchemy
+$ pipenv shell
+$ touch app.py
+```
---
+++ app.py
@@ -0,0 +1,107 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+import os
+
+
+## Init app
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Database Init 
+db = SQLAlchemy(app)
+
+# Init Marshmallow
+ma = Marshmallow(app)
+
+class Car(db.Model):
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    colour = db.Column(db.String(20))
+
+    def __init__(self, name, description, price, colour):
+        self.name = name 
+        self.description = description
+        self.price = price 
+        self.colour = colour
+
+# @app.route('/', methods=['GET'])
+# def get():
+#     return jsonify({ 'msg': 'Hello - this is a test get'})
+
+# Car Schema 
+class CarSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'description', 'price', 'colour')
+
+## Init Car Schema 
+car_schema = CarSchema()
+cars_schema = CarSchema()
+
+# Create a car
+@app.route('/car', methods=['POST'])
+def add_car():
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    colour = request.json['colour']  
+
+    new_car = Car(name, description, price, colour) 
+
+    db.session.add(new_car)
+    db.session.commit()
+
+    return car_schema.jsonify(new_car)
+
+# Get all cars
+@app.route('/car', methods=['GET'])
+def get_all_cars():
+    all_cars = Car.query.all()
+    result = cars_schema.dump(all_cars,many=True)
+    return jsonify(result)
+
+# Get single car
+@app.route('/car/<id>', methods=['GET'])
+def get_car(id):
+    car = Car.query.get(id)
+    return car_schema.jsonify(car)
+
+
+# Update a car
+@app.route('/car/<id>', methods=['PUT'])
+def update_car(id):
+    car = Car.query.get(id)
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    colour = request.json['colour']  
+
+    car.name = name 
+    car.description = description
+    car.price = price 
+    car.colour = colour 
+
+
+    db.session.commit()
+
+    return car_schema.jsonify(car)
+
+# Delete single car
+@app.route('/car/<id>', methods=['DELETE'])
+def delete_car(id):
+    car = Car.query.get(id)
+    db.session.delete(car)
+    db.session.commit()
+    return car_schema.jsonify(car)
+
+
+
+if __name__ == '__main__': 
+    app.run(debug=True)
